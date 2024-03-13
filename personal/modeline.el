f98356a901d6b7d29559beca655d7a763ed3f9b2
Barthelemy Renucci <b.renucci@pm.me>
2024-03-13 12:24:12 +0100

    [personal] Add ui and modeline

---
+++ personal/modeline.el
@@ -0,0 +1,13 @@
+;;; Modeline file
+
+
+(prelude-require-packages '(doom-modeline nerd-icons))
+
+(require 'doom-modeline)
+(doom-modeline-mode 1)
+(setq doom-modeline-column-zero-based nil)
+(setq doom-modeline-buffer-encoding nil)
+(setq doom-modeline-workspace-name nil)
+(setq doom-modeline-lsp nil)
+(setq doom-modeline-gnus nil)
+(setq doom-modeline-irc nil)
