2554410855eb32196933253ca7a8148e84b525de
Barthelemy Renucci <b.renucci@pm.me>
2024-03-13 12:25:19 +0100

    [personal] Add keybindings & misc

---
+++ personal/misc.el
@@ -0,0 +1,8 @@
+;;; Misc file
+
+;; Please leave me alone
+(setq prelude-guru nil)
+
+;; Dumbjump
+(setq xref-show-definitions-function #'xref-show-definitions-completing-read)
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
