a74c13a0020e3de5957f0044f2f69699841aa4ea
Barthelemy Renucci <b.renucci@pm.me>
2024-03-13 12:24:41 +0100

    [personal] Add smart tabs mode

---
+++ personal/cc-mode.el
@@ -0,0 +1,11 @@
+;;; C mode
+
+(prelude-require-packages '(smart-tabs-mode))
+
+(smart-tabs-insinuate 'c)
+(setq-default tab-width 4) ; or any other preferred value
+(setq cua-auto-tabify-rectangles nil)
+
+(add-hook 'c-mode-common-hook
+          (lambda () (setq indent-tabs-mode t)))
+
