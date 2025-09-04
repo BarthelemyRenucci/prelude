5efc473e94c65c9335698160820576b62da222c7
Barthelemy Renucci <b.renucci@pm.me>
2025-09-04 16:41:50 +0200

    [ui] Rework transparency

--- personal/ui.el
+++ personal/ui.el
@@ -31,11 +31,9 @@
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
 ;; Set transparency of emacs
-(defun transparency (value)
-  "Sets the transparency of the frame window. 0=transparent/100=opaque"
-  (interactive "nTransparency Value 0 - 100 opaque:")
-  (set-frame-parameter (selected-frame) 'alpha value))
-(transparency 95)
+
+(set-frame-parameter nil 'alpha-background 85)
+(add-to-list 'default-frame-alist '(alpha-background . 85))
 
 
 ;; Add nyan-cat
