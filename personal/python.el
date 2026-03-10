6aa6bc8c99213c43c42bd3ae9be7e040413cb339
Barthelemy Renucci <b.renucci@pm.me>
2026-03-10 11:36:06 +0100

    [personal][python.el] Add alphelia and ruff format

--- personal/python.el
+++ personal/python.el
@@ -1,8 +1,28 @@
 ;;; Python
 
-(prelude-require-package 'python-black)
+(prelude-require-packages '(apheleia company-jedi python-pytest toml-mode))
 
-(use-package python-black
+(require 'company)
+(require 'flycheck)
+
+(use-package apheleia
   :demand t
-  :after python
-  :hook (python-mode . python-black-on-save-mode))
+  :config
+  (setf (alist-get 'ruff apheleia-formatters)
+        '("sh" "-c" "ruff check --fix --force-exclude --exit-zero --stdin-filename \"$1\" - | ruff format --stdin-filename \"$1\" -" "--" filepath))
+  (setf (alist-get 'python-mode apheleia-mode-alist) 'ruff)
+  (apheleia-global-mode +1))
+
+(defun my/python-mode-hook ()
+  (add-to-list 'company-backends 'company-jedi)
+  ;; Ensure flycheck is active
+  (flycheck-mode +1)
+  ;; Use ruff as the default checker
+  (setq-local flycheck-checker 'python-ruff))
+
+(add-hook 'python-mode-hook 'my/python-mode-hook)
+
+(use-package python-pytest
+  :bind (("C-c x" . python-pytest-dispatch))
+  :custom
+  (python-pytest-confirm t))
