f5e9fb84ceecc5e626e06b125aac6527b6c72091
Barthelemy Renucci <b.renucci@pm.me>
2026-03-10 11:36:21 +0100

    [personal][cc-mode.el] Disable alphelia for c mode

--- personal/cc-mode.el
+++ personal/cc-mode.el
@@ -5,7 +5,10 @@
 (setq cua-auto-tabify-rectangles nil)
 
 (add-hook 'c-mode-common-hook
-          (lambda () (setq indent-tabs-mode t)))
+          (lambda ()
+            (setq indent-tabs-mode t)
+            (when (fboundp 'apheleia-mode)
+              (apheleia-mode -1))))
 
 ;; Switch case offset
 (c-set-offset 'case-label '+)
