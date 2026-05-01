00ab0eb9db72997d7d66fc3063f4bcf0544e7f23
Barthelemy Renucci <barthelemy.renucci@fifteen.eu>
2026-05-01 11:25:05 +0200

    [personal][keybindings.el] Modified term to "zh" keychord

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -103,4 +103,4 @@
 (key-chord-define-global "jj" 'avy-goto-char-timer)
 (key-chord-define-global "kk" 'avy-goto-line)
 (key-chord-define-global "ii" 'magit-blame-addition)
-(key-chord-define-global "zz" 'vterm)
+(key-chord-define-global "zh" 'vterm)
